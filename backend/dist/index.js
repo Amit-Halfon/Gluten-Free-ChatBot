d135bd220953838fd217e51f178ea8a78eaa8980
Amit-halfon <halfon1@mail.tau.ac.il>
2025-05-05 20:14:43 +0300

    started working on the backend a bit

---
+++ backend/dist/index.js
@@ -0,0 +1,10 @@
+import app from "./app.js";
+import { connectToDatabase } from "./db/connection.js";
+//connections and listeners
+const PORT = process.env.PORT || 5000;
+connectToDatabase()
+    .then(() => {
+    app.listen(PORT, () => console.log("Server Open & Connected To Database"));
+})
+    .catch((error) => console.log(error));
+//# sourceMappingURL=index.js.map
